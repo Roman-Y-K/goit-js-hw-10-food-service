2006482bae0bbba39a40de751a39bcf958a13f97
Roman-Y-K <r.prohor7@gmail.com>
2020-12-04 21:02:46 +0200

    исправил темы

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import css from "./css/styles.css";
-import "./js/menu"
-import "./js/change-theme"
+import "./css/styles.css";
+import "./js/menu";
+import "./js/change-theme";
+import "./js/theme";
--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -1,4 +1,4 @@
-import { Theme } from './theme';
+import { Theme } from "./theme";
 
 
 const checkboxRef = document.querySelector('#theme-switch-toggle');
@@ -7,34 +7,42 @@ const bodyRef = document.querySelector('body');
 
 bodyRef.addEventListener('change', changeBodyTheme);
 
+
+bodyRef.classList.add(Theme.LIGHT)
 savedBodyTheme();
-
-
 
 
 function changeBodyTheme() {
     if (checkboxRef.checked) {
+        console.log(1);
         onDarkTheme();
     } else
         onLightTheme();
 };
 
+
+
 function onDarkTheme() {
-    bodyRef.classList.replace(Theme.LIGHT, Theme.DARK) && localStorage.setItem('theme', Theme.DARK);
+    bodyRef.classList.replace(Theme.LIGHT, Theme.DARK)  && localStorage.setItem('theme', Theme.DARK);
+    console.log(2);
     
 };
 
 function onLightTheme() {
-     bodyRef.classList.replace(Theme.DARK, Theme.LIGHT) && localStorage.setItem('theme', Theme.LIGHT);
+    console.log(3);
+     bodyRef.classList.replace(Theme.DARK, Theme.LIGHT)  && localStorage.setItem('theme', Theme.LIGHT);
 };
 
 function savedBodyTheme() {
     const savedTheme = localStorage.getItem('theme');
+    console.log(4);
     
-    bodyRef.classList.add(savedTheme);
+    bodyRef.classList.replace(Theme.LIGHT, savedTheme);
+    console.log(5);
     
     if (bodyRef.classList.contains(Theme.DARK)) { 
         checkboxRef.checked = true;
+        console.log(6);
     }
 };
 
