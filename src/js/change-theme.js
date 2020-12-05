e112e029f158983c5ba66b64eda193733fe0fcd2
Roman-Y-K <r.prohor7@gmail.com>
2020-12-05 10:25:59 +0200

    исправления

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -8,41 +8,48 @@ const bodyRef = document.querySelector('body');
 bodyRef.addEventListener('change', changeBodyTheme);
 
 
-bodyRef.classList.add(Theme.LIGHT)
-savedBodyTheme();
+bodyRef.classList.add(Theme.LIGHT);
 
 
 function changeBodyTheme() {
     if (checkboxRef.checked) {
-       
-        onDarkTheme();
+       onDarkTheme();
     } else
-       {onLightTheme()} ;
+       onLightTheme();
 };
 
+savedBodyTheme();
 
 
 function onDarkTheme() {
-    bodyRef.classList.replace(Theme.LIGHT, Theme.DARK)  && localStorage.setItem('theme', Theme.DARK);
-
+    bodyRef.classList.replace( Theme.LIGHT, Theme.DARK), setLocalStorageDark();
     
 };
 
 function onLightTheme() {
-  
-     bodyRef.classList.replace(Theme.DARK, Theme.LIGHT)  && localStorage.setItem('theme', Theme.LIGHT);
+    bodyRef.classList.replace(Theme.DARK, Theme.LIGHT), setLocalStorageLight();
 };
 
 function savedBodyTheme() {
+
     const savedTheme = localStorage.getItem('theme');
    
-    
-    bodyRef.classList.replace(Theme.LIGHT, savedTheme);
- 
-    
-    if (bodyRef.classList.contains(Theme.DARK)) { 
+    if (savedTheme === Theme.DARK) {
+        bodyRef.classList.replace(Theme.LIGHT, savedTheme);
+         
+    };
+
+    if (bodyRef.classList.contains(Theme.DARK)) {
         checkboxRef.checked = true;
      
-    }
+    };
 };
 
+function setLocalStorageDark() {
+    localStorage.setItem('theme', Theme.DARK);
+};
+
+function setLocalStorageLight() {
+    localStorage.setItem('theme', Theme.LIGHT);
+    
+}
